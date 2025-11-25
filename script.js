9a13f4df6ef75f42daa9187069067ccc368cff79
hussain-anjum <haratul14@gmail.com>
2025-11-25 23:46:27 +0600

    Implement basic API call to fetch weather data

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const apiKey = "138d2b5787ac50a34456242045231530";
 const apiURL =
   "https://api.openweathermap.org/data/2.5/weather?q=bangladesh&units=metric";
+
+async function checkWeather() {
+  const response = await fetch(apiURL + `&appid=${apiKey}`);
+  let data = await response.json();
+
+  console.log(data);
+}
+checkWeather();
